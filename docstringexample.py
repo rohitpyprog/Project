ce1137f81e3b07dc5a4657c3fd4d35c699a50972
rohitpyprog <rohitdhakarey777@gmail.com>
2024-12-25 13:44:54 +0530

    other progress

---
+++ docstringexample.py
@@ -0,0 +1,5 @@
+def cube(n):
+    """give a number and return the cube of that number"""
+    return n**3
+print(cube(56))
+print(cube.__doc__)
---
+++ fstring.py
@@ -0,0 +1,6 @@
+# x=float(input("enter your percentage"))
+# y=f"My percentage is {x:.2f}"
+# print(y)
+f=f"{2*30}"
+print(f)
+print(type(f))
